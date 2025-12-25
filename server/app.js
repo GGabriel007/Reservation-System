ebddcfce03ba94ac17e61b73966d3c8a7baaa0cd
Samuel Nye <samuelnyevr@gmail.com>
2025-12-24 21:32:31 -0800

    more modification for server upload

--- server/app.js
+++ server/app.js
@@ -1,14 +1,15 @@
 import express from "express";
-import cors from "cors";
+// import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log("Hello World");
+  res.send("hello world");
 });
 
 export default app;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 import app from "./app.js";
-import { connectDB } from "./config/db.js";
+// import { connectDB } from "./config/db.js";
 
 const PORT = 5050;
 
-connectDB();
+// connectDB();
 
 app.listen(PORT, () => {
   console.log(`SERVER RUNNING ON PORT ${PORT}`);
